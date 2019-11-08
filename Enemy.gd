b2e243c4fbf64c6645666b623ec829ee7459743a
luke <jleldridge27@gmail.com>
2019-11-07 18:47:03 -0800

    Add red enemy sprites.

--- Enemy.gd
+++ Enemy.gd
@@ -4,21 +4,25 @@ const MIN_SPEED = 100
 const MAX_SPEED = 300
 const MIN_SIZE = 1
 const MAX_SIZE = 250
+const DEFAULT_SPRITE_SCALE = 0.1
 
 var speed = Vector2()
 var size
-    
+
+func _ready():
+	$AnimatedSprite.play()
+
 func determine_size(player_size):
     speed.x = -1 * rand_range(MIN_SPEED, MAX_SPEED)
     size = rand_range(MIN_SIZE, min(MAX_SIZE, player_size + 50))
     _scaleSize()
     
 func _scaleSize():
-    var computedSize = 0.5 + 0.02 * size
+    var computedSize = DEFAULT_SPRITE_SCALE + 0.02 * size
     $CollisionShape2D.scale.x = computedSize
     $CollisionShape2D.scale.y = computedSize
-    $Sprite.scale.x = computedSize
-    $Sprite.scale.y = computedSize
+    $AnimatedSprite.scale.x = computedSize
+    $AnimatedSprite.scale.y = computedSize
 
 func _physics_process(delta):
     move_and_collide(speed * delta)
